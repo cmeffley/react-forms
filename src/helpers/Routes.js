8c665506380545fc6f5d0eab362688713d95abec
Chris Meffley <cmeffley@gmail.com>
2021-05-01 14:42:31 -0400

    show single card with with dynamic routing

--- src/helpers/Routes.js
+++ src/helpers/Routes.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 import Home from '../views/Home';
 import Students from '../views/Students';
 import AddStudent from '../views/AddStudent';
+import SingleStudent from '../views/SingleStudent';
+import NotFound from '../views/NotFound';
 
 export default function Routes({ students, setStudents }) {
   return (
@@ -11,13 +13,19 @@ export default function Routes({ students, setStudents }) {
       <Switch>
         <Route exact path='/' component={Home} />
         <Route
-          path='/students'
+          exact path='/students'
           component={() => <Students students={students} setStudents={setStudents} />}
+        />
+        <Route
+          path='/students/:firebaseKey'
+          component={SingleStudent}
         />
         <Route
           path='/add-students'
           component={() => <AddStudent setStudents={setStudents} />}
         />
+        <Route path='*' component={NotFound} />
+        {/* // Not found component HAS TO BE AT THE BOTTOM, the star means any path */}
       </Switch>
     </div>
   );
