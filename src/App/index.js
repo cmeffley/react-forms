80f27fe1a76c196dcb47df32e95e3690a0677ed4
Chris Meffley <cmeffley@gmail.com>
2021-05-03 20:02:00 -0400

    added firebase auth

--- src/App/index.js
+++ src/App/index.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from '../components/NavBar';
 import Routes from '../helpers/Routes';
@@ -6,15 +8,32 @@ import { getStudents } from '../helpers/data/StudentData';
 
 function App() {
   const [students, setStudents] = useState([]);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     getStudents().then((response) => setStudents(response));
   }, []);
 
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((authed) => { // authed is a taco
+      if (authed) {
+        const userInfoObject = { // example of stuff that you might need from Google data
+          fullName: authed.displayName,
+          profileImage: authed.photoURL,
+          uid: authed.uid,
+          user: authed.email.split('@')[0]
+        };
+        setUser(userInfoObject);
+      } else if (user || user === null) { // null shows when app is loading
+        setUser(false);
+      }
+    });
+  }, []);
+
   return (
     <>
       <Router>
-        <Navbar />
+        <Navbar user={user}/>
         <Routes
           students={students}
           setStudents={setStudents}
